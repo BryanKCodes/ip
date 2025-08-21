0e0fe07b5d1c4f4720c0512391dac08b6411f0c7
BryanKCodes <bryankoh1228@gmail.com>
2025-08-22 00:05:56 +0800

    feat: implement marking and unmarking functionality for tasks

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -11,16 +11,34 @@ public class Parser {
      * @return true if program should continue, false if it should exit.
      */
     public boolean handle(String input) {
-        switch(input.toLowerCase()) {
-            case "list":
-                Ui.list(taskList);
-                return true;
-            case "bye":
-                Ui.exit();
-                return false;
-            default:
-                Ui.add(taskList, input);
-                return true;
+        String command = input.toLowerCase();
+        if (command.equals("bye")) {
+            Ui.exit();
+            return false;
+        } else if (command.equals("list")) {
+            Ui.list(taskList);
+        } else if (command.startsWith("mark ")) {
+            int id = Integer.parseInt(input.split(" ")[1]) - 1;
+            Task task = taskList.getTask(id);
+            if (task == null) {
+                Ui.showMessage("Unable to find task " + (id + 1));
+            } else {
+                task.markAsDone();
+                Ui.showMessage("Nice! I've marked this task as done:\n  " + task);
+            }
+        } else if (command.startsWith("unmark ")) {
+            int id = Integer.parseInt(input.split(" ")[1]) - 1;
+            Task task = taskList.getTask(id);
+            if (task == null) {
+                Ui.showMessage("Unable to find task " + (id + 1));
+            } else {
+                task.markAsUndone();
+                Ui.showMessage("OK, I've marked this task as not done yet:\n  " + task);
+            }
+        } else {
+            Ui.add(taskList, input);
         }
+
+        return true;
     }
 }
