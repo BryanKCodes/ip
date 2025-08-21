2f38151735bc787c944ae612063296a2f504b860
BryanKCodes <bryankoh1228@gmail.com>
2025-08-21 22:10:55 +0800

    feat: create TaskList to store tasks

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,21 @@
+public class TaskList {
+    private String[] tasks = new String[100];
+    private int taskCount = 0;
+
+    public void addTask(String task) {
+        tasks[taskCount++] = task;
+    }
+
+    @Override
+    public String toString() {
+        if (taskCount == 0) {
+            return "No tasks";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < taskCount; i++) {
+            sb.append(i + 1).append(". ").append(tasks[i]).append("\n");
+        }
+        return sb.toString();
+    }
+}
