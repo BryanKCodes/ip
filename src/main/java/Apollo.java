f4b29b7ae15e678402a3006c368cbc37a589d14f
BryanKCodes <bryankoh1228@gmail.com>
2025-08-21 20:46:39 +0800

    feat: add greet and exit functionality

--- src/main/java/Apollo.java
+++ src/main/java/Apollo.java
@@ -1,10 +1,21 @@
 public class Apollo {
+    public static void showMessage(String message) {
+        String border = "____________________________________________________________";
+        System.out.println(border);
+        System.out.println(message);
+        System.out.println(border);
+    }
+
+    public static void greet() {
+        showMessage("Hello! I'm Apollo\nWhat can I do for you?");
+    }
+
+    public static void exit() {
+        showMessage("Bye. Hope to see you again soon!");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        exit();
     }
 }
