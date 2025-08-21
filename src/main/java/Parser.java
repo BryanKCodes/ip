49f329985321637f232e4b583c6cb13b71bc1d08
BryanKCodes <bryankoh1228@gmail.com>
2025-08-22 01:29:56 +0800

    feat: add detailed input validation and error messages

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -30,61 +30,81 @@ public class Parser {
         Matcher matcher;
 
         // MARK
-        matcher = Pattern.compile("^mark\\s+(\\d+)$", Pattern.CASE_INSENSITIVE).matcher(input);
-        if (matcher.matches()) {
-            int id = Integer.parseInt(matcher.group(1)) - 1;
-            Task task = taskList.getTask(id);
-            if (task == null) {
-                Ui.showMessage("Unable to find task " + (id + 1));
+        if (input.toLowerCase().startsWith("mark")) {
+            matcher = Pattern.compile("^mark\\s+(\\d+)$", Pattern.CASE_INSENSITIVE).matcher(input);
+            if (matcher.matches()) {
+                int id = Integer.parseInt(matcher.group(1)) - 1;
+                Task task = taskList.getTask(id);
+                if (task == null) {
+                    Ui.showMessage("Unable to find task " + (id + 1));
+                } else {
+                    task.markAsDone();
+                    Ui.showMessage("Nice! I've marked this task as done:\n  " + task);
+                }
             } else {
-                task.markAsDone();
-                Ui.showMessage("Nice! I've marked this task as done:\n  " + task);
+                Ui.showMessage("Were you trying to mark tasks? The correct format is: mark <task number>");
             }
             return;
         }
 
         // UNMARK
-        matcher = Pattern.compile("^unmark\\s+(\\d+)$", Pattern.CASE_INSENSITIVE).matcher(input);
-        if (matcher.matches()) {
-            int id = Integer.parseInt(matcher.group(1)) - 1;
-            Task task = taskList.getTask(id);
-            if (task == null) {
-                Ui.showMessage("Unable to find task " + (id + 1));
+        if (input.toLowerCase().startsWith("unmark")) {
+            matcher = Pattern.compile("^unmark\\s+(\\d+)$", Pattern.CASE_INSENSITIVE).matcher(input);
+            if (matcher.matches()) {
+                int id = Integer.parseInt(matcher.group(1)) - 1;
+                Task task = taskList.getTask(id);
+                if (task == null) {
+                    Ui.showMessage("Unable to find task " + (id + 1));
+                } else {
+                    task.markAsUndone();
+                    Ui.showMessage("OK, I've marked this task as not done yet:\n  " + task);
+                }
             } else {
-                task.markAsUndone();
-                Ui.showMessage("OK, I've marked this task as not done yet:\n  " + task);
+                Ui.showMessage("Were you trying to unmark tasks? The correct format is: unmark <task number>");
             }
             return;
         }
 
         // TODO
-        matcher = Pattern.compile("^todo\\s+(.+)$", Pattern.CASE_INSENSITIVE).matcher(input);
-        if (matcher.matches()) {
-            Task task = new ToDo(matcher.group(1));
-            taskList.addTask(task);
-            Ui.add(task, taskList.size());
+        if (input.toLowerCase().startsWith("todo")) {
+            matcher = Pattern.compile("^todo\\s+(.+)$", Pattern.CASE_INSENSITIVE).matcher(input);
+            if (matcher.matches()) {
+                Task task = new ToDo(matcher.group(1));
+                taskList.addTask(task);
+                Ui.add(task, taskList.size());
+            } else {
+                Ui.showMessage("The description of a todo cannot be empty. Format: todo <description>");
+            }
             return;
         }
 
         // DEADLINE
-        matcher = Pattern.compile("^deadline\\s+(.+)\\s+/by\\s+(.+)$", Pattern.CASE_INSENSITIVE).matcher(input);
-        if (matcher.matches()) {
-            Task task = new Deadline(matcher.group(1), matcher.group(2));
-            taskList.addTask(task);
-            Ui.add(task, taskList.size());
+        if (input.toLowerCase().startsWith("deadline")) {
+            matcher = Pattern.compile("^deadline\\s+(.+)\\s+/by\\s+(.+)$", Pattern.CASE_INSENSITIVE).matcher(input);
+            if (matcher.matches()) {
+                Task task = new Deadline(matcher.group(1), matcher.group(2));
+                taskList.addTask(task);
+                Ui.add(task, taskList.size());
+            } else {
+                Ui.showMessage("The correct format for a deadline is: deadline <description> /by <time>");
+            }
             return;
         }
 
         // EVENT
-        matcher = Pattern.compile("^event\\s+(.+)\\s+/from\\s+(.+)\\s+/to\\s+(.+)$", Pattern.CASE_INSENSITIVE).matcher(input);
-        if (matcher.matches()) {
-            Task task = new Event(matcher.group(1), matcher.group(2), matcher.group(3));
-            taskList.addTask(task);
-            Ui.add(task, taskList.size());
+        if (input.toLowerCase().startsWith("event")) {
+            matcher = Pattern.compile("^event\\s+(.+)\\s+/from\\s+(.+)\\s+/to\\s+(.+)$", Pattern.CASE_INSENSITIVE).matcher(input);
+            if (matcher.matches()) {
+                Task task = new Event(matcher.group(1), matcher.group(2), matcher.group(3));
+                taskList.addTask(task);
+                Ui.add(task, taskList.size());
+            } else {
+                Ui.showMessage("The correct format for an event is: event <description> /from <start> /to <end>");
+            }
             return;
         }
 
         // fallback
-        Ui.showMessage("Sorry, I can't process your message. Please try again.");
+        Ui.showMessage("Sorry, I do not understand your message. Please try again.");
     }
 }
